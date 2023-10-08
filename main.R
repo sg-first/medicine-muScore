907a5c10874dbdab1c0caa4f56cd17cb07e9856f
3A <524019351@qq.com>
2023-10-08 16:30:38 +0800

    完成

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-library(mustat)
+library(muStat)
 library(readxl)
 
 oldScore = read_excel("baseline.xlsx", sheet = '评分信息')
@@ -6,3 +6,17 @@ oldScore = oldScore$Score
 NL = read_excel("baseline.xlsx", sheet = 'NL')
 LS = read_excel("baseline.xlsx", sheet = 'LS')
 xueqing = read_excel("baseline.xlsx", sheet = '血清')
+
+NL = data.frame(NL$CCL17, NL$CCL18, NL$CCL22, NL$CCL27, NL$`IFN-γ`, NL$IL13, NL$IL18, 
+                NL$IL19, NL$IL31, NL$IL33, NL$IL4, NL$MMP12)
+LS = data.frame(LS$CCL17, LS$CCL18, LS$CCL22, LS$CCL27, LS$`IFN-γ`, LS$IL13, LS$IL18, 
+                LS$IL19, LS$IL31, LS$IL33, LS$IL4, LS$MMP12)
+xueqing = data.frame(xueqing$CCL17, xueqing$CCL18, xueqing$CCL22, xueqing$CCL27, xueqing$`IFN-γ`, xueqing$IL13, xueqing$IL18, 
+                     xueqing$IL19, xueqing$IL31, xueqing$IL33, xueqing$IL4, xueqing$MMP12)
+
+NLScore = mu.score(NL)
+LSScore = mu.score(LS)
+xueqingScore = mu.score(xueqing)
+
+scoreDf = data.frame(oldScore, NLScore, LSScore, xueqingScore)
+print(cor(scoreDf))
---
+++ readme.md
@@ -0,0 +1,12 @@
+medicine-muScore
+==========
+每个sheet的mu score数据在`baselineScore.RData`
+
+相关性结果（只需要看第一行/第一列）
+```
+                oldScore   NLScore     LSScore xueqingScore
+oldScore      1.00000000 0.1377908 -0.03274839   0.50236479
+NLScore       0.13779078 1.0000000  0.10082491   0.11722701
+LSScore      -0.03274839 0.1008249  1.00000000  -0.01147381
+xueqingScore  0.50236479 0.1172270 -0.01147381   1.00000000
+```
